681fdef6d739b1dd3465e26f7601a333baaf4140
seerdomin <seerdomin@gmail.com>
2015-07-31 01:04:14 +0200

    [test] Add a suite for the 'forward all' use case

--- test.js
+++ test.js
@@ -4,11 +4,7 @@ var test = require('tape');
 var sinon = require('sinon');
 var forward = require('./');
 
-if (process.env.NODE_ENV && process.env.NODE_ENV === 'coverage') {
-    test.createStream();
-}
-
-test('String-based forwarding', function(t) {
+test('Forwarding one of the provider\'s methods', function(t) {
     var receiver, provider, methods, spy;
 
     methods = 'circle';
@@ -16,47 +12,37 @@ test('String-based forwarding', function(t) {
     receiver = {};
 
     provider = {
-        'circle': function circle(){},
-        'square': function square(){},
-        'triangle': function triangle(){}
+        'circle': function circle() {
+            return provider;
+        }
     };
 
     spy = sinon.spy(provider, methods);
-
-    t.comment('## forward(<object>, <object>, <string>)');
 
     forward(receiver, provider, methods);
 
     t.ok(
         isMethod(receiver, 'circle'), 
-        'Calling forward() should create methods whose name match the "methods" string'
-    );
-
-    t.ok(
-        (!hasProp(receiver, 'square') && !hasProp(receiver, 'triangle')), 
-        'Calling forward() should not create methods whose name does not match the "methods" string'
+        'When calling forward(receiver, provider, method) : the receiver should proxy one of the provider\'s methods'
     );
 
     receiver.circle();
 
     t.ok(
         spy.calledOnce, 
-        'Calling a forwarded method on the receiver should call the same method on the provider'
+        'Calling the forwarded method on the receiver should call the same method on the provider'
     );
-
-    t.comment(' ');
 
     t.end();
 });
 
 
-test('Array-based forwarding', function(t) {
+test('Forwarding a subset of the provider\'s methods', function(t) {
     var receiver, provider, methods, spySquare, spyTriangle;
 
     receiver = {};
 
     provider = {
-        'circle': function circle(){},
         'square': function square(){},
         'triangle': function triangle(){}
     };
@@ -66,19 +52,11 @@ test('Array-based forwarding', function(t) {
     spySquare = sinon.spy(provider, 'square');
 
     spyTriangle = sinon.spy(provider, 'triangle');
-
-    t.comment('## forward(<object>, <object>, <array>)');
-
     forward(receiver, provider, methods);
 
     t.ok(
         (isMethod(receiver, 'square') && isMethod(receiver, 'triangle')), 
-        'Calling forward() should create methods whose names are in the "methods" array'
-    );
-
-    t.ok(
-        !hasProp(receiver, 'circle'), 
-        'Calling forward() should not create methods whose names are not in the "methods" array'
+        'When calling forward(receiver, provider, methods) : the receiver should proxy a subset of the provider\'s methods'
     );
 
     receiver.square();
@@ -94,9 +72,43 @@ test('Array-based forwarding', function(t) {
 });
 
 
-function hasProp(obj, prop) {
-    return obj.hasOwnProperty(prop);
-}
+test('Forwarding all of the provider\'s methods', function(t) {
+    var receiver, provider, spyCircle, spySquare, spyTriangle;
+
+    receiver = {};
+
+    provider = {
+        'circle': function circle(){},
+        'square': function square(){},
+        'triangle': function triangle(){}
+    };
+
+    spyCircle = sinon.spy(provider, 'circle');
+    
+    spySquare = sinon.spy(provider, 'square');
+
+    spyTriangle = sinon.spy(provider, 'triangle');
+
+    forward(receiver, provider);
+
+    t.ok(
+        (isMethod(receiver, 'circle') && isMethod(receiver, 'square') && isMethod(receiver, 'triangle')), 
+        'When calling forward(receiver, provider) : the receiver should proxy all of the provider\'s methods'
+    );
+
+    receiver.circle();
+    
+    receiver.square();
+
+    receiver.triangle();
+
+    t.ok(
+        (spyCircle.calledOnce && spySquare.calledOnce && spyTriangle.calledOnce), 
+        'Calling forwarded methods on the receiver should call the same methods on the provider'
+    );
+
+    t.end();
+});
 
 function isMethod(obj, prop) {
     return (typeof obj[prop] === 'function');
